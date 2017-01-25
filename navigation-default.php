ee6f0f5a774c29cb7291b0115b1c0881c401ce5a
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-25 18:46:29 +0800

    update navbar

--- navigation-default.php
+++ navigation-default.php
@@ -10,6 +10,8 @@ $main_nav_options = array(
 );
 ?>
 
+
+<?php /*
 <?php if ( has_nav_menu( 'main_menu' ) ) : ?>
   <nav class="navbar navbar-light bg-faded">
     <div class="container">
@@ -22,4 +24,27 @@ echo '<img alt="Brand" src="'.$image[0].'"/>'; ?>
       
     </div><!-- .container -->
   </nav>
-<?php endif; ?>
+<?php endif; ?> */ ?>
+
+
+
+
+
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+          <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+echo '<img alt="Brand" src="'.$image[0].'"/>'; ?>
+        </a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <?php wp_nav_menu( $main_nav_options ); ?>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
