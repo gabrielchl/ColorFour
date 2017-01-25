3d35ad9c0d3506edeeed5ce9ef8baa2262a5cc22
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-25 12:20:24 +0800

    create new page types

---
+++ page-contact.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Contact Page
+ * Description: Page template only for contact page.
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'navigation', 'default' ); ?>
+
+<div class="container main-container">
+  <div class="row">
+<?php
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+      get_template_part( 'content', get_post_format() );
+    endwhile;
+      bootstrap_four_the_posts_pagination();
+  else :
+    ?><p><?php _e( 'Sorry, no posts matched your criteria.', 'bootstrap-four' ); ?></p><?php
+  endif;
+?>
+  </div><!-- .row -->
+</div><!-- .container.main-container -->
+
+<?php get_footer(); ?>
