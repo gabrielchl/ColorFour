7472820b610a896235d52cf3805a09a50403d5a5
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-25 19:06:40 +0800

    reset navbar

--- navigation-default.php
+++ navigation-default.php
@@ -4,7 +4,7 @@ $main_nav_options = array(
   'depth'             => 2,
   'container'         => '',
   'container_class'   => '',
-  'menu_class'        => 'nav navbar-nav navbar-right',
+  'menu_class'        => 'navbar-nav mr-auto',
   'fallback_cb'       => 'bootstrap_four_wp_navwalker::fallback',
   'walker'            => new bootstrap_four_wp_navwalker()
 );
@@ -29,8 +29,8 @@ echo '<img alt="Brand" src="'.$image[0].'"/>'; ?>
 
 
 
-
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<?php /*
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded" style="margin-bottom:0;">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -42,6 +42,31 @@ echo '<img alt="Brand" src="'.$image[0].'"/>'; ?>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <?php wp_nav_menu( $main_nav_options ); ?>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav> */ ?>
+
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
