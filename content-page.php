c7a08f7e8e8fea0865938a3d0951841f205c24c4
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-25 21:12:36 +0800

    udpate style

--- content-page.php
+++ content-page.php
@@ -5,19 +5,20 @@
  * @package _tk
  */
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if (is_home()==true):?>
-    <header>
-		<h1 class="page-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-<?php endif; ?>
-	<div class="entry-content">
-		<div class="entry-content-thumbnail">
-			<?php the_post_thumbnail(); ?>
-		</div>
-		<?php the_content(); ?>
-		<?php _tk_link_pages(); ?>
-	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', '_tk' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-</article><!-- #post-## -->
+    <article style="margin-bottom:0;" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php if (is_home()==true):?>
+            <header>
+                <h1 class="page-title"><?php the_title(); ?></h1> </header>
+            <!-- .entry-header -->
+            <?php endif; ?>
+                <div class="entry-content">
+                    <div class="entry-content-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                    <?php the_content(); ?>
+                        <?php _tk_link_pages(); ?>
+                </div>
+                <!-- .entry-content -->
+                <?php edit_post_link( __( 'Edit', '_tk' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+    </article>
+    <!-- #post-## -->
