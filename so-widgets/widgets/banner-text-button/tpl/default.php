1aded6b21066c915bb127dd86c16533f28aaf1a5
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-26 13:18:05 +0800

    minor fix

--- so-widgets/widgets/banner-text-button/tpl/default.php
+++ so-widgets/widgets/banner-text-button/tpl/default.php
@@ -1,18 +1,16 @@
 <div class="row" style="padding-top:40px; padding-bottom:40px;">
-    <div class="container">
-        <div class="container">
-            <div class="" style="color: #FE8453; font-size:48px;">
+    <div class="container" style="padding-bottom:0;">
+        <div class="container" style="padding-bottom:0;">
+            <h2 style="color: #FE8453; font-size:48px;">
                 <?php echo $instance['banner-text-button-text-one'];?>
-            </div>
-            <div class="" style="color: #FE8453;">
+            </h2>
+            <p style="color: #FE8453;">
                 <?php echo $instance['banner-text-button-text-two'];?>
-            </div>
-            <div class="">
-                <center id="changethisprop">
-                    <a href="<?php echo $instance['banner-text-button-button-link'];?>" style="background-color: #FE8453; color:#fff; padding: 9px 30px;">
-                        <?php echo $instance['banner-text-button-button-text'];?>
-                    </a>
-                </center>
+            </p>
+            <div style="margin-top:20px;">
+                <a href="<?php echo $instance['banner-text-button-button-link'];?>" style="background-color: #FE8453; color:#fff; padding: 9px 30px;">
+                    <?php echo $instance['banner-text-button-button-text'];?>
+                </a>
             </div>
         </div>
     </div>
