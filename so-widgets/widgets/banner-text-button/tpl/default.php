72e610770a494c817b4916d69075178ae68671f6
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-26 13:12:26 +0800

    update banner text

--- so-widgets/widgets/banner-text-button/tpl/default.php
+++ so-widgets/widgets/banner-text-button/tpl/default.php
@@ -1,26 +1,19 @@
 <div class="row" style="padding-top:40px; padding-bottom:40px;">
-    <div class="container" id="getthisheiht">
+    <div class="container">
         <div class="container">
-            <div class="col-md-5 col-sm-12 col-xs-12" style="color: #FE8453; font-size:48px;">
+            <div class="" style="color: #FE8453; font-size:48px;">
                 <?php echo $instance['banner-text-button-text-one'];?>
             </div>
-            <div class="col-md-4 col-sm-8 col-xs-12" style="color: #FE8453;">
+            <div class="" style="color: #FE8453;">
                 <?php echo $instance['banner-text-button-text-two'];?>
             </div>
-            <div class="col-md-3 col-sm-4 col-xs-12">
+            <div class="">
                 <center id="changethisprop">
                     <a href="<?php echo $instance['banner-text-button-button-link'];?>" style="background-color: #FE8453; color:#fff; padding: 9px 30px;">
                         <?php echo $instance['banner-text-button-button-text'];?>
                     </a>
                 </center>
             </div>
-            <script>
-                //var element = document.getElementById('getthisheiht');
-                //var thisheight = element.offsetHeight;
-                //var myElement = document.getElementById('changethisprop');
-                //thisheight += "px";
-                //myElement.style.lineHeight = thisheight;
-            </script>
         </div>
     </div>
 </div>
