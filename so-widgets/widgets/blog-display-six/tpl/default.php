289bcbe06a704b169c6ed3ff2bdee2dd007c81fe
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-27 09:22:30 +0800

    minor fix

--- so-widgets/widgets/blog-display-six/tpl/default.php
+++ so-widgets/widgets/blog-display-six/tpl/default.php
@@ -1,7 +1,7 @@
 <div class="row" style="">
     <div class="container">
         <div class="row">
-            <?php /*
+            <?php 
             global $post;
             
 $myposts = get_posts( array( 'posts_per_page' => 6, 'orderby' => 'date', 'order' => 'DSC', 'post_type'  => 'post', ) );
@@ -10,7 +10,7 @@ $myposts = get_posts( array( 'posts_per_page' => 6, 'orderby' => 'date', 'order'
             if ( $myposts ) {
         foreach ( $myposts as $post ) : 
             setup_postdata( $post ); ?>
-                <div class="col-md-6 col-xl-4 newholder">
+                <div class="col-md-4 col-sm-6 newholder">
                     <div class="newsimg" style="width:100%; height: 200px; background-image:url('<?php if ( has_post_thumbnail() ) {
     the_post_thumbnail_url(); } else { echo 'http://gabriellee.esy.es/awanahkdemo/wp-content/themes/awanahk/awanalogo.png';}?>'); background-position:center; background-size:contain; background-color:#bbb; background-repeat:no-repeat;"></div>
                     <h5 class="newstitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -18,19 +18,9 @@ $myposts = get_posts( array( 'posts_per_page' => 6, 'orderby' => 'date', 'order'
                 <?php
         endforeach;
         wp_reset_postdata();
-    } */
+    } 
             
             ?>
-                    <?php
-$args = array( 'numberposts' => 6, 'order'=> 'ASC', 'orderby' => 'date' );
-$postslist = get_posts( $args );
-foreach ($postslist as $post) :  setup_postdata($post); ?>
-                        <div class="col-md-4 col-sm-6 newholder">
-                            <div class="newsimg" style="width:100%; height: 200px; background-image:url('<?php if ( has_post_thumbnail() ) {
-    the_post_thumbnail_url(); } else { echo 'http://gabriellee.esy.es/awanahkdemo/wp-content/themes/awanahk/awanalogo.png';}?>'); background-position:center; background-size:contain; background-color:#bbb; background-repeat:no-repeat;"></div>
-                            <h5 class="newstitle"><a href="<?php get_permalink($recent["ID"]); ?>"><?php the_title(); ?></a></h5>
-                            <h6 class="newsdate"><?php the_date(); ?></h6> </div>
-                        <?php endforeach;wp_reset_postdata(); ?>
         </div>
     </div>
     <style>
