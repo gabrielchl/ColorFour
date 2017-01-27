39910fb8a17cbce0d7b510627d5244622a2fec01
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-27 10:27:14 +0800

    test

--- so-widgets/widgets/blog-display-six/tpl/default.php
+++ so-widgets/widgets/blog-display-six/tpl/default.php
@@ -6,12 +6,12 @@
 $posts = new WP_Query($query);
 while($posts->have_posts()) : $posts->the_post(); ?>
                 <div class="col-md-4 col-sm-6 newholder">
-                    <div class="newsimg" style="width:100%; height: 200px; background-image:url('<?php if ( has_post_thumbnail() ) {
+                    <div onclick="location.href='<?php the_permalink(); ?>';" class="newsimg" style="width:100%; height: 200px; background-image:url('<?php if ( has_post_thumbnail() ) {
     the_post_thumbnail_url(); } else { $custom_logo_id = get_theme_mod( 'custom_logo' );
 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 echo $image[0]; }?>'); background-position:center; background-size:contain; background-color:#bbb; background-repeat:no-repeat;"></div>
-                    <h5 class="newstitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <h6 class="newsdate"><?php echo get_the_date(); ?></h6> </div>
+                    <h5 class="newstitle" style="font-weight:600; font-size:17px;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <h6 class="newsdate" style="margin-top:4px;"><?php echo get_the_date(); ?></h6> </div>
                 <?php endwhile; wp_reset_postdata(); ?>
         </div>
     </div>
