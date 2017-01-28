22abbc1194f12b4539839386876f5b896eec6b69
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-01-28 08:58:40 +0800

    minor fix

--- so-widgets/widgets/four-contact-boxes/tpl/default.php
+++ so-widgets/widgets/four-contact-boxes/tpl/default.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6 col-xs-6 four-contact-boxes four-contact-boxes-one">
-            <div class="contact-box-icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+            <div class="contact-box-icon"><i class="contact-fa fa fa-map-marker" aria-hidden="true" syle="color:#FE8453"></i></div>
             <div class="contact-box-content">
                 <p class="contact-box-content-text">
                     <?php echo $instance['contact-box-address']; ?>
@@ -9,15 +9,17 @@
             </div>
         </div>
         <div class="col-sm-6 col-xs-6 four-contact-boxes four-contact-boxes-two">
-            <div class="contact-box-icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+            <div class="contact-box-icon"><i class="contact-fa fa fa-phone" aria-hidden="true" syle="color:#80C3D3"></i></div>
             <div class="contact-box-content">
                 <p class="contact-box-content-text">
                     <?php echo $instance['contact-box-tel']; ?>
                 </p>
             </div>
         </div>
+    </div>
+    <div class="row">
         <div class="col-sm-6 col-xs-6 four-contact-boxes four-contact-boxes-three">
-            <div class="contact-box-icon"><i class="fa fa-fax" aria-hidden="true"></i></div>
+            <div class="contact-box-icon"><i class="contact-fa fa fa-fax" aria-hidden="true" syle="color:#88C87A"></i></div>
             <div class="contact-box-content">
                 <p class="contact-box-content-text">
                     <?php echo $instance['contact-box-fax']; ?>
@@ -25,7 +27,7 @@
             </div>
         </div>
         <div class="col-sm-6 col-xs-6 four-contact-boxes four-contact-boxes-four">
-            <div class="contact-box-icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+            <div class="contact-box-icon"><i class="contact-fa fa fa-envelope-o" aria-hidden="true" syle="color:#F1CD3B"></i></div>
             <div class="contact-box-content">
                 <p class="contact-box-content-text">
                     <?php echo $instance['contact-box-email']; ?>
@@ -34,3 +36,12 @@
         </div>
     </div>
 </div>
+<style>
+    .contact-fa {
+        font-size: 70px;
+    }
+    
+    .four-contact-boxes {
+        text-align: center;
+    }
+</style>
