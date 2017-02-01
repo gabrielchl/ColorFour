eb30021484bd98d49c13e72e3c1510c32f227b56
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-02-01 21:19:34 +0800

    404 more updae

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@ get_header(); ?>
             <div class="container">
                 <div style="color: #555;">
                     <?php
-                    $whattodispnum = rand(1,3);
+                    $whattodispnum = rand(1,7);
                     switch ($whattodispnum) {
                         case 1:
                             $printfourofour = 'Nothing Here?!';
@@ -22,14 +22,30 @@ get_header(); ?>
                         case 3:
                             $printfourofour = 'Oh No!!!';
                             break;
+                        case 4:
+                            $printfourofour = 'What have you done?';
+                            break;
+                        case 5:
+                            $printfourofour = 'Oops';
+                            break;
+                        case 6:
+                            $printfourofour = 'We\'re sorry';
+                            break;
+                        case 7:
+                            $printfourofour = 'How come!';
+                            break;
                     }
                     ?>
                         <h1 style="font-weight: bold; font-size:70px;"><?php echo $printfourofour; ?></h1>
                         <p style="font-weight: 500;">The page you've been looking for has gone... :'(
-                            <br>Maybe you would like to get back?</p>
+                            <br>Maybe you would like to <a href="#" onclick="goBack()">get back?</a></p>
                 </div>
             </div>
+            <script>
+                function goBack() {
+                    window.history.back();
+                }
+            </script>
             <!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
         </div>
-        <?php get_sidebar(); ?>
-            <?php get_footer(); ?>
+        <?php get_footer(); ?>
