bb7ad278b6b6d5b1f9325e2963584165dd2e46b5
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-02-01 21:13:32 +0800

    40

--- 404.php
+++ 404.php
@@ -6,23 +6,30 @@
  */
 
 get_header(); ?>
-
-	<?php // add the class "panel" below here to wrap the content-padder in Bootstrap style ;) ?>
-	<section class="content-padder error-404 not-found">
-
-		<header>
-			<h2 class="page-title"><?php _e( 'Oops! Something went wrong here.', '_tk' ); ?></h2>
-		</header><!-- .page-header -->
-
-		<div class="page-content">
-
-			<p><?php _e( 'Nothing could be found at this location. Maybe try a search?', '_tk' ); ?></p>
-
-			<?php get_search_form(); ?>
-
-		</div><!-- .page-content -->
-
-	</section><!-- .content-padder -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+    <?php // add the class "panel" below here to wrap the content-padder in Bootstrap style ;) ?>
+        <div class="row" style="margin-top: 100px; margin-bottom: 100px;">
+            <div class="container">
+                <div style="color: #555;">
+                    <?php
+                    $whattodispnum = rand(1,3);
+                    switch ($whattodispnum) {
+                        case 1:
+                            $printfourofour = 'Nothing Here?!';
+                            break;
+                        case 2:
+                            $printfourofour = 'It\'s BLANK!';
+                            break;
+                        case 3:
+                            $printfourofour = 'Oh No!!!';
+                            break;
+                    }
+                    ?>
+                        <h1 style="font-weight: bold; font-size:70px;">Nothing Here?!</h1>
+                        <p style="font-weight: 500;">The page you've been looking for has gone... :'(
+                            <br>Maybe you would like to get back?</p>
+                </div>
+            </div>
+            <!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+        </div>
+        <?php get_sidebar(); ?>
+            <?php get_footer(); ?>
