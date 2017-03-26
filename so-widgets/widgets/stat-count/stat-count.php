530a841f5cac4aa4593f5573b1eb508a1435e1a7
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-03-26 14:16:54 +0800

    update

--- so-widgets/widgets/stat-count/stat-count.php
+++ so-widgets/widgets/stat-count/stat-count.php
@@ -61,11 +61,3 @@ class Stat_Count extends SiteOrigin_Widget {
 }
 
 siteorigin_widget_register('stat-count', __FILE__, 'Stat_Count');
-
-function stat_count_banner_img_src( $banner_url, $widget_meta ) {
-    if( $widget_meta['ID'] == 'stat-count') {
-        $banner_url = plugin_dir_url(__FILE__) . 'stat_count_banner.png';
-    }
-    return $banner_url;
-}
-add_filter( 'siteorigin_widgets_widget_banner', 'stat_count_banner_img_src', 10, 2);
