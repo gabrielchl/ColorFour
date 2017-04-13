d61ae689eeb89936bdd382fc2711e330b3816297
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-13 11:57:31 +0800

    update widget

--- so-widgets/widgets/three-stat-count/tpl/default.php
+++ so-widgets/widgets/three-stat-count/tpl/default.php
@@ -26,3 +26,42 @@
         font-size: 16px;
     }
 </style>
+<div class="col-sm-4">
+    <div class="stat_count_box">
+        <div class="stat_count_num" style="color:<?php echo $instance['three-stat-count-number-color-one'];?>;">
+            <h1>
+            <?php echo $instance['three-stat-count-number-one'];?>
+        </h1> </div>
+        <div class="stat_count_text" style="color:<?php echo $instance['three-stat-count-text-color-one'];?>;">
+            <p>
+                <?php echo $instance['three-stat-count-text-one'];?>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+    <div class="stat_count_box">
+        <div class="stat_count_num" style="color:<?php echo $instance['three-stat-count-number-color-two'];?>;">
+            <h1>
+            <?php echo $instance['three-stat-count-number-two'];?>
+        </h1> </div>
+        <div class="stat_count_text" style="color:<?php echo $instance['three-stat-count-text-color-two'];?>;">
+            <p>
+                <?php echo $instance['three-stat-count-text-two'];?>
+            </p>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+    <div class="stat_count_box">
+        <div class="stat_count_num" style="color:<?php echo $instance['three-stat-count-number-color-three'];?>;">
+            <h1>
+            <?php echo $instance['three-stat-count-number-three'];?>
+        </h1> </div>
+        <div class="stat_count_text" style="color:<?php echo $instance['three-stat-count-text-color-three'];?>;">
+            <p>
+                <?php echo $instance['three-stat-count-text-three'];?>
+            </p>
+        </div>
+    </div>
+</div>
