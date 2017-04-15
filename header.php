258c50143ec46f9ff95c5be12edb9e3d40b5f845
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-15 19:37:03 +0800

    load jquery\

--- header.php
+++ header.php
@@ -48,6 +48,7 @@
                 <!-- .container -->
                 </header>
                 <!-- #masthead -->*/ ?>
+                <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
                 <div class="upon-nav"></div>
                 <nav class="site-navigation">
                     <?php // substitute the class "container-fluid" below if you want a wider content area ?>
