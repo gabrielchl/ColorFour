0e8cd8e72c779fe7f05eb2c5d9ea70267183602f
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-15 21:28:57 +0800

    update

--- footer.php
+++ footer.php
@@ -171,9 +171,22 @@
                 });
             </script>
             <script>
-                var maxHeight = Math.max.apply(null, $("div.sameHeight").map(function () {
-                    return $(this).height();
-                }).get());
+                $(document).ready(function () {
+                    // Select and loop the container element of the elements you want to equalise
+                    $('.four_to_two').each(function () {
+                        // Cache the highest
+                        var highestBox = 0;
+                        // Select and loop the elements you want to equalise
+                        $('.four-link-box', this).each(function () {
+                            // If this box is higher than the cached highest then store it
+                            if ($(this).height() > highestBox) {
+                                highestBox = $(this).height();
+                            }
+                        });
+                        // Set the height of all those children to whichever was highest 
+                        $('.four-link-box', this).height(highestBox);
+                    });
+                });
             </script>
     </footer>
     <!-- close #colophon -->
