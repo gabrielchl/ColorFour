b1907b259a178090a5e0605aa091e00472e2249d
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-20 15:58:50 +0800

    update search page

--- page_search.php
+++ page_search.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package _tk
- */
+/*
+Template Name: Search Page
+*/
 
 get_header(); ?>
     <?php get_search_form(); ?>
