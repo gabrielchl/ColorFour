a4bc81c148b919dd012398c3b1d13382752cbe3e
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-20 16:03:40 +0800

    update

--- searchform.php
+++ searchform.php
@@ -5,9 +5,12 @@
  * @package _tk
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_tk' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', '_tk' ); ?>">
-	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_tk' ); ?>">
-</form>
+    <form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+        <div class="form-group">
+            <label for="searchitem">
+                <input id="searchitem" type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_tk' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', '_tk' ); ?>"> </label>
+            <button type="submit" class="search-submit btn btn-default">
+                <?php echo esc_attr_x( 'Search', 'submit button', '_tk' ); ?>
+            </button>
+        </div>
+    </form>
