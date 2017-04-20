94d3baab51adec62f6ef98b6ccd10499c4e90056
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-20 16:04:57 +0800

    update

--- search.php
+++ search.php
@@ -6,27 +6,16 @@
  */
 
 get_header(); ?>
-
-	<?php if ( have_posts() ) : ?>
-
-		<header>
-			<h2 class="page-title"><?php printf( __( 'Search Results for: %s', '_tk' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
-		</header><!-- .page-header -->
-
-		<?php // start the loop. ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'content', 'search' ); ?>
-
-		<?php endwhile; ?>
-
-		<?php _tk_pagination(); ?>
-
-	<?php else : ?>
-
-		<?php get_template_part( 'no-results', 'search' ); ?>
-
-	<?php endif; // end of loop. ?>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+    <?php if ( have_posts() ) : ?>
+        <header>
+            <h2 class="page-title"><?php printf( __( 'Search Results for: %s', '_tk' ), '<span>' . get_search_query() . '</span>' ); ?></h2> </header>
+        <!-- .page-header -->
+        <?php // start the loop. ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'content', 'search' ); ?>
+                    <?php endwhile; ?>
+                        <?php _tk_pagination(); ?>
+                            <?php else : ?>
+                                <?php get_template_part( 'no-results', 'search' ); ?>
+                                    <?php endif; // end of loop. ?>
+                                        <?php get_footer(); ?>
