1dafa71c3b73ed2d1e8b5f35113bea98320e7a52
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-04-20 15:56:51 +0800

    update

---
+++ page_search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package _tk
+ */
+
+get_header(); ?>
+    <?php get_search_form(); ?>
+        <?php // get_sidebar(); ?>
+            <?php get_footer(); ?>
