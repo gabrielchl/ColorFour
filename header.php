5c0d18b69db97e3fd39b8a1b60f0804b84ae9827
Gabriel Chi Hong Lee <chihonglee777@gmail.com>
2017-05-17 20:47:34 +0800

    fix

--- header.php
+++ header.php
@@ -11,6 +11,21 @@
     <html <?php language_attributes(); ?>>
 
     <head>
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+            ga('create', 'UA-97306293-1', 'auto');
+            ga('require', 'GTM-K588L6D');
+            ga('send', 'pageview');
+        </script>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
